68c6bfb9073a38a93be6a2a7fb73bc07d8f2b751
Sha <exssasha@gmail.com>
2025-05-16 08:28:15 +0800

    Update database settings for PythonAnywhere deployment

--- website/wsgi.py
+++ website/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
+import sys
+project_home = '/home/ShaWmgxe4/grrc_final/website/app'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
